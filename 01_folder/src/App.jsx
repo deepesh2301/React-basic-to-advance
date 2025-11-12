6324d4a7ad4c4ae45fca6c602b512ef3d049eec0
deepesh2301 <deepeshsahu9840@gmail.com>
2025-11-12 23:48:08 +0530

    01_foldder

---
+++ 01_folder/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='raju'>Hello raju</h1>
+      <h2 className='shyam'>Hello shyam</h2>
+    </div>
+  )
+}
+
+export default App
